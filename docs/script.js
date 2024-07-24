c11564401d231ff8bbed6c885e71ff2b52f739a9
CappCat <abiudvera123@gmail.com>
2024-07-23 19:46:05 -0500

    Creación del repositorio

---
+++ docs/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('btn1').addEventListener('click', function() {
+        window.location.href = 'https://github.com/Andioh15/Examen-Final-Alex';
+    });
+
+    document.getElementById('btn2').addEventListener('click', function() {
+        window.location.href = 'https://github.com/Andioh15/Bingo';
+    });
+
+    document.getElementById('btn3').addEventListener('click', function() {
+        window.location.href = 'https://github.com/Andioh15/Sistema-de-facturaci-n.-Objetos-y-abstracci-n-de-datos.';
+    });
+});
